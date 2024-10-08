e8f962202d168a7c9d96d29d0ecc86d26a45676c
Yasinbimbam <49526065+Yasinbimbam@users.noreply.github.com>
2024-10-08 17:59:19 +0000

    added st-pages dependency

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,21 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+from st_pages import add_page_title, get_nav_from_toml
+
+st.set_page_config(layout="wide")
+
+# If you want to use the no-sections version, this
+# defaults to looking in .streamlit/pages.toml, so you can
+# just call `get_nav_from_toml()`
+nav = get_nav_from_toml(".streamlit/pages_sections.toml")
+
+st.logo("logo.png")
+
+pg = st.navigation(nav)
+
+add_page_title(pg)
+
+pg.run()
